85e749eebc9a6cb76e3f2f92faf54152aec20ce5
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-01-30 20:14:08 +0300

    lab - 03 - done beautify

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -10,7 +10,7 @@ public class Test1 {
     @DisplayName("Name method works correctly")
     void Name() {
         val context = new ClassPathXmlApplicationContext("application-context.xml");
-        Person person = context.getBean("person", Person.class);
+        Person person = context.getBean("person1", Person.class);
 
         System.out.println(person);
     }
